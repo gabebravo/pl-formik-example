c639ba4ce2fe08e0ed80d460866c2e7462517d34
Gabe Bravo <gabecbravo@gmail.com>
2019-07-28 09:54:45 -0400

    removed console logs

--- src/redux/store.js
+++ src/redux/store.js
@@ -9,14 +9,15 @@ const prefix = 'USER_STORE/';
 
 // USER
 export const setUserData = createActions(`${prefix}SET_USER_DATA`);
+console.log('setUserData', setUserData);
 
 const reducers = handleActions(
   {
     // USER
-    [setUserData]: (state, { payload }) => ({
-      ...state,
-      userData: payload.data
-    })
+    [setUserData]: (state, { payload }) => {
+      console.log('payload', payload);
+      return { ...state, userData: payload.data };
+    }
   },
   defaultState
 );
@@ -29,6 +30,7 @@ export const fetchUserData = ({ email, callback }) => (dispatch, getState) => {
       if (data.length > 0) {
         const [user] = data;
         const transformedUser = transformUser(user);
+        dispatch(setUserData({ data: transformedUser }));
         callback(transformedUser);
       }
     })
